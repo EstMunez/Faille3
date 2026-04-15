ce6fd0a1625c47279a6e4cfc5993d66552141fa6
EstMunez <esther.mwiseneza_munezero@ifosup.wavre.be>
2026-04-15 15:27:55 +0200

    Add new origin to CORS whitelist

--- api/self_modules/middlewares/cors.js
+++ api/self_modules/middlewares/cors.js
@@ -1,4 +1,4 @@
-var whitelist = ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:3001']
+var whitelist = ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:3001','https://capable-jalebi-067730.netlify.app']
 
 module.exports = corsOptions = {
   origin: function (origin, callback) {
