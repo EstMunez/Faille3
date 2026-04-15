640be3ead6c9a358c2fb3969abbdef48cd3cd2f5
EstMunez <esther.mwiseneza_munezero@ifosup.wavre.be>
2026-04-15 18:26:56 +0200

    Change API URL from localhost to production

--- 40bierges/src/views/blog/blog.jsx
+++ 40bierges/src/views/blog/blog.jsx
@@ -19,7 +19,7 @@ class Blog extends React.Component {
       messages: [],
       token: "",
       isLoading: true,
-      url: "http://localhost:3001"
+      url: "https://faille3.onrender.com"
     };
     this.handleChange = this.handleChange.bind(this)
     this.handleSend = this.handleSend.bind(this)
